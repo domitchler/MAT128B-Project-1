4c882f1b5b74e48a7332c46e87076d24ab797b3c
smpoindexter <36578827+smpoindexter@users.noreply.github.com>
2018-02-18 16:55:51 -0800

    Julia Set code for unit disk

--- juliaSet.m
+++ juliaSet.m
@@ -1,13 +1,13 @@
-phi = inline('z^2 - 1.25');   % Define the function whose fixed points we seek.
-fixpt1 = (1 + sqrt(6))/2;     % These are the fixed points.
-fixpt2 = (1 - sqrt(6))/2;
+phi = inline('z^2');   % Define the function whose fixed points we seek.
+fixpt1 = 0;     % These are the fixed points.
+fixpt2 = 1;
 
 colormap([1 0 0; 1 1 1]);     % Points numbered 1 (inside) will be colored red;
                               %   those numbered 2 (outside) will be colored white.
-M = 2*ones(141,361);          % Initialize array of point colors to 2 (white).
+M = 2*ones(241,361);          % Initialize array of point colors to 2 (white).
 
-for j=1:141,                  % Try initial values with imaginary parts between
-  y = -.7 + (j-1)*.01;        %   -0.7 and 0.7
+for j=1:241,                  % Try initial values with imaginary parts between
+  y = -1.2 + (j-1)*.01;        %   -1.2 and 1.2
   for i=1:361,                % and with real parts between
     x = -1.8 + (i-1)*.01;     %   -1.8 and 1.8.
     z = x + 1i*y;             % 1i is the MATLAB symbol for sqrt(-1).
@@ -38,5 +38,5 @@ for j=1:141,                  % Try initial values with imaginary parts between
   end;
 end;
 
-image([-1.8 1.8],[-.7 .7],M),  % This plots the results.
+image([-1.8 1.8],[-1.2 1.2],M),  % This plots the results.
 axis xy                        % If you don't do this, vertical axis is inverted.
