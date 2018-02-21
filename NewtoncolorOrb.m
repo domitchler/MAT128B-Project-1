2bd7730ec1b16918bf3a37ad886b541375daa257
blee7374 <36247271+blee7374@users.noreply.github.com>
2018-02-21 10:10:06 -0800

    Add files via upload
    Used the code from ColoredOrb to see if the Newton Method can be applied in the Complex Plane. Tried replacing the fixed point iteration with the Newton Method but had difficulty trying to differentiate the inline function for the f prime part of the Newton Method equation

---
+++ NewtoncolorOrb.m
@@ -0,0 +1,21 @@
+function NewtoncolorOrb(phi)
+phi = inline('z.^3 - 1');
+colormap(parula(100));
+M = zeros(241,361);
+for j=1:241                  % Try initial values with imaginary parts between
+    y = -1.2 + (j-1)*.01;        %   -1.2 and 1.2
+    for i=1:361                % and with real parts between
+        x = -1.8 + (i-1)*.01;     %   -1.8 and 1.8.
+        z = x + 1i*y;             % 1i is the MATLAB symbol for sqrt(-1).
+        kount = 0;                % kount is the total number of iterations.
+        
+        while kount < 100 && abs(z) <= 100
+            kount = kount+1;
+            z = z - (phi(z)/(3*z^2));           %This is Newton's Method
+        end
+        M(j,i) = kount;
+    end
+end
+image([-1.8 1.8],[-1.2 1.2],M),  % This plots the results.
+axis xy                        % If you don't do this, vertical axis is inverted.
+end
